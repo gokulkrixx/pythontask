7535ff7593390269973608501e3f7a0f35c5bf18
gokulkrixx <gk503255@gmail.com>
2025-09-17 10:30:12 +0530

    Add initial implementation of volume calculation script

---
+++ day1classtask.py
@@ -0,0 +1,10 @@
+
+x = 15.5
+y = 20
+z = 10.25
+total =  x+y+z
+print(f"total volume ={total}")
+print(f"total volume to an integer ={int(total)}")
+print(f"total volume to string ={str(total)}")
+import random
+print(f"final total after bouns={random.randrange(5,10)+total}")
