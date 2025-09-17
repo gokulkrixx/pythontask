baf544f843fb95b61236de216aa7db320803d844
gokulkrixx <gk503255@gmail.com>
2025-09-17 22:05:49 +0530

    Add inventory management functions and item counting logic

---
+++ day7hw.py
@@ -0,0 +1,23 @@
+inventory = []
+def add_item(item):
+    inventory.append(item)
+
+def count_items(items):
+    if not items:
+        return 0
+    return 1 + count_items(items[1:])
+
+def main():
+    add_item("dog food")
+    add_item("cat food")
+    add_item("fish food")
+    add_item("bird cage")
+
+list = lambda x :print(f"item:{x}")
+
+for item in inventory:
+    list(item)
+
+total = count_items(inventory)
+print(f"total number of items:{total}")
+
